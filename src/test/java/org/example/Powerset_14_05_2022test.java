27a2013dde8bd9e18d1929270049fa659d4cc067
srisindhu1 <104442042+srisindhu1@users.noreply.github.com>
2022-05-15 23:31:30 +0530

    problem37test

---
+++ src/test/java/org/example/Powerset_14_05_2022test.java
@@ -0,0 +1,18 @@
+package dailycodingproblems;
+
+import org.junit.Test;
+
+public class Powerset_14_05_2022test {
+    @Test
+    public void test1(){
+        char[] set={'a','b','c'};
+        Powerset_14_05_2022.printset(set,3);
+
+    }
+    @Test
+    public void test2(){
+        char[] set={'1','2','3','4'};
+        Powerset_14_05_2022.printset(set,4);
+
+    }
+}
