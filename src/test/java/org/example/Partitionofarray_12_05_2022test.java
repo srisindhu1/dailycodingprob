9031a1c1f3982c4ce07a4e9af7e1cfcb9f4266a5
srisindhu1 <104442042+srisindhu1@users.noreply.github.com>
2022-05-15 23:25:23 +0530

    problem35test

---
+++ src/test/java/org/example/Partitionofarray_12_05_2022test.java
@@ -0,0 +1,12 @@
+package dailycodingproblems;
+
+import org.junit.Test;
+
+public class Partitionofarray_12_05_2022test {
+    @Test
+    public void test1(){
+        char[] arr={'G', 'B', 'R', 'R', 'B', 'R', 'G'};
+        char[] a=Partitionofarray_12_05_2022.partition(arr);
+        System.out.println(a);
+    }
+}
