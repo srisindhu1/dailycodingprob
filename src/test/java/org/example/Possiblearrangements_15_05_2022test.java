db2d65707eab46102a7e48da8864a6e8f12fe1e9
srisindhu1 <104442042+srisindhu1@users.noreply.github.com>
2022-05-16 12:55:35 +0530

    problem38test

---
+++ src/test/java/org/example/Possiblearrangements_15_05_2022test.java
@@ -0,0 +1,14 @@
+package dailycodingproblems;
+
+import org.junit.Test;
+
+public class Possiblearrangements_15_05_2022test {
+    @Test
+    public  void test1(){
+        Possiblearrangements_15_05_2022.nsolution(4);
+    }
+    @Test
+    public void test2(){
+        Possiblearrangements_15_05_2022.nsolution(2);
+    }
+}
