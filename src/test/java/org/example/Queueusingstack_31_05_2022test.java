cbde397c0e1e1256b6e220aa413b9929e5e036e5
srisindhu1 <104442042+srisindhu1@users.noreply.github.com>
2022-06-01 15:33:41 +0530

    problem53test

---
+++ src/test/java/org/example/Queueusingstack_31_05_2022test.java
@@ -0,0 +1,16 @@
+package dailycodingproblems;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Queueusingstack_31_05_2022test {
+    @Test
+    public void test1(){
+        Queueusingstacks_31_05_2022 queue=new Queueusingstacks_31_05_2022();
+        queue.enqueue(3);
+        queue.enqueue(6);
+        queue.enqueue(9);
+
+    }
+}
