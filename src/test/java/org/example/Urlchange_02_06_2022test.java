8e06959deb043e341981a17c48d8d60b3cce1821
srisindhu1 <104442042+srisindhu1@users.noreply.github.com>
2022-06-02 14:26:42 +0530

    problem56test

---
+++ src/test/java/org/example/Urlchange_02_06_2022test.java
@@ -0,0 +1,11 @@
+package dailycodingproblems;
+
+import org.junit.Test;
+
+public class Urlchange_02_06_2022test {
+    @Test
+    public void test1(){
+        Urlchange_02_06_2022 url=new Urlchange_02_06_2022(1000);
+        Urlchange_02_06_2022.encode("www.google.com");
+    }
+}
