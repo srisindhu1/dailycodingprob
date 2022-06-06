146c4a4cacdcaeab2761172cb9901d3cbfee1faf
srisindhu1 <104442042+srisindhu1@users.noreply.github.com>
2022-06-06 11:29:33 +0530

    problem58test

---
+++ src/test/java/org/example/Findnum_06_06_2022test.java
@@ -0,0 +1,13 @@
+package dailycodingproblems;
+
+import org.junit.Test;
+
+public class Findnumber_06_06_2022test {
+    @Test
+    public void test1(){
+        int[] arr={12,4,5,6,7,8,9};
+        int num=8;
+        Findnumber_06_06_2022.findnum(arr,num);
+
+    }
+}
