5b154ed337221a7d71c05e7beef359191ded2982
srisindhu1 <104442042+srisindhu1@users.noreply.github.com>
2022-06-08 11:54:08 +0530

    problem60test

---
+++ src/test/java/org/example/Halfsum_07_06_2022test.java
@@ -0,0 +1,16 @@
+package dailycodingproblems;
+
+import org.junit.Test;
+
+public class halfsum_07_06_2022test {
+    @Test
+    public void test1(){
+        int[] arr={15,5,10,20,50};
+        halfsum_07_06_2022.find(arr);
+    }
+    @Test
+    public void test2(){
+        int[] arr={1,2,3,4,5,6};
+        halfsum_07_06_2022.find(arr);
+    }
+}
