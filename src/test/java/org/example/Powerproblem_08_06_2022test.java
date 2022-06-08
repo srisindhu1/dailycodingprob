26db1dd33284cb7cdf1a398c35f4fe00ceebd7ac
srisindhu1 <104442042+srisindhu1@users.noreply.github.com>
2022-06-08 11:55:13 +0530

    problem61test

---
+++ src/test/java/org/example/Powerproblem_08_06_2022test.java
@@ -0,0 +1,11 @@
+package dailycodingproblems;
+
+import org.junit.Test;
+
+public class Powerproblem_08_06_2022test {
+    @Test
+    public void test1(){
+        Powerproblem_08_06_2022.power(2,10);
+
+    }
+}
