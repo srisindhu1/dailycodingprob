d7719d5fc964ba22f2b673ca58f54fe172981de7
srisindhu1 <104442042+srisindhu1@users.noreply.github.com>
2022-06-09 13:02:01 +0530

    problem62test

---
+++ src/test/java/org/example/Paths_09_06_2022test.java
@@ -0,0 +1,15 @@
+package dailycodingproblems;
+
+
+import org.junit.Test;
+
+public class Paths_09_06_2022test {
+    @Test
+    public void test1(){
+        Paths_09_06_2022.numberofpaths(2,2);
+    }
+    @Test
+    public void test2(){
+        Paths_09_06_2022.numberofpaths(5,5);
+    }
+}
