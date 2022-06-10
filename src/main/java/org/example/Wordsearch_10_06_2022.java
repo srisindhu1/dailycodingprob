e3123b3b7b4a21dd3d8c2f4bc65ca3b3f86aafb4
srisindhu1 <104442042+srisindhu1@users.noreply.github.com>
2022-06-10 14:57:10 +0530

    problem63

--- src/main/java/org/example/Wordsearch_10_06_2022.java
+++ src/main/java/org/example/Wordsearch_10_06_2022.java
@@ -1,5 +1,12 @@
 package dailycodingproblems;
-
+//Given a 2D matrix of characters and a target word, write a function that returns whether the word can be found in the matrix by 
+// going left-to-right, or up-to-down.
+//For example, given the following matrix:
+//[['F', 'A', 'C', 'I'],
+// ['O', 'B', 'Q', 'P'],
+// ['A', 'N', 'O', 'B'],
+// ['M', 'A', 'S', 'S']]
+//and the target word 'FOAM', you should return true,
 public class Wordsearch_10_06_2022 {
     static int[] x={1,0};
     static int[] y={0,1};
