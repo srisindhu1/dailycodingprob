9bce0326f1f68a0afc4999cc0cd3759bfbec49a7
srisindhu1 <104442042+srisindhu1@users.noreply.github.com>
2022-06-13 12:27:08 +0530

    problem65test

---
+++ src/test/java/org/example/Spiralloop_12_06_2022test.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+public class Spiralloop_12_06_2022test {
+    @Test
+    public void testCase_1(){
+        int m=4,n=4;
+        int[][] arr={ { 1, 2, 3, 4 },
+                { 5, 6, 7, 8 },
+                { 9, 10, 11, 12 },
+                { 13, 14, 15, 16 } };
+        Spiralloop_12_06_2022.spiralprint(m,n,arr);
+    }
+
+}
