9329aaaacb0bc2823d22e4599ae422371b8acf26
srisindhu1 <104442042+srisindhu1@users.noreply.github.com>
2022-06-15 15:40:57 +0530

    problem63test

--- src/test/java/org/example/Wordsearch_10_06_2022test.java
+++ src/test/java/org/example/Wordsearch_10_06_2022test.java
@@ -1,6 +1,6 @@
 package dailycodingproblems;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class Wordsearch_10_06_2022test {
     @Test
@@ -9,8 +9,22 @@ public class Wordsearch_10_06_2022test {
                 {'O', 'B', 'Q', 'P'},
                 {'A', 'N', 'O', 'B'},
                 {'M', 'A', 'S', 'S'}} ;
-        String word="FOAM";
-        Wordsearch_10_06_2022.search(arr,word);
+        int val= arr.length;
+        String word="MASS";
+        boolean a=Wordsearch_10_06_2022.checkMatch(arr,word,val,val);
+        System.out.println(a);
     }
+    @Test
+    public  void test2(){
+        char[][] arr={{'F', 'A', 'C', 'I'},
+                {'O', 'B', 'Q', 'P'},
+                {'A', 'N', 'O', 'B'},
+                {'M', 'A', 'S', 'S'}} ;
+        int val=arr.length;
+        String word="NOSS";
+        boolean a=Wordsearch_10_06_2022.checkMatch(arr,word,val,val);
+        System.out.println(a);
+    }
+
 
 }
