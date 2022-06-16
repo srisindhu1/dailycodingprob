9d922ea3459f272d07033cc9c8aadd46b99b3392
srisindhu1 <104442042+srisindhu1@users.noreply.github.com>
2022-06-16 17:37:56 +0530

    problem69test

---
+++ src/test/java/org/example/Maxprod_15_06_2022test.java
@@ -0,0 +1,13 @@
+package dailycodingproblems;
+
+import org.junit.jupiter.api.*;
+
+public class Maxprod_15_06_2022test {
+    @Test
+    public void test1(){
+        int arr[] = {-10, -3, 5, 6, -20};
+        int n = arr.length;
+        System.out.println(Maxprod_15_06_2022.maxProduct(arr,n));
+    }
+
+}
