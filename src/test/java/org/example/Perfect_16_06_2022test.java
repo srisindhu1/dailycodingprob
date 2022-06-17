3588741a546834294552d71133285b8c32fe112c
srisindhu1 <104442042+srisindhu1@users.noreply.github.com>
2022-06-17 14:47:11 +0530

    problem70test

---
+++ src/test/java/org/example/Perfect_16_06_2022test.java
@@ -0,0 +1,27 @@
+package dailycodingproblems;
+
+import org.junit.jupiter.api.Test;
+
+public class Perfect_16_06_2022test {
+    @Test
+    public void test1(){
+        int a=Perfect_16_06_2022.check(1);
+        System.out.println(a);
+    }
+    @Test
+    public void test2(){
+        int a=Perfect_16_06_2022.check(10);
+        System.out.println(a);
+    }
+    @Test
+    public void test3(){
+        int a=Perfect_16_06_2022.check(6);
+        System.out.println(a);
+    }
+    @Test
+    public void test4(){
+        int a=Perfect_16_06_2022.check(20);
+        System.out.println(a);
+    }
+}
+
