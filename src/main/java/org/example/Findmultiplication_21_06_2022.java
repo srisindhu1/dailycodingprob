70d67fd24e9e6c82d36bdf03419be19a4f743b95
srisindhu1 <104442042+srisindhu1@users.noreply.github.com>
2022-06-21 11:23:00 +0530

    problem74

---
+++ src/main/java/org/example/Findmultiplication_21_06_2022.java
@@ -0,0 +1,28 @@
+package dailycodingproblems;
+//Suppose you have a multiplication table that is N by N. That is, a 2D array
+// where the value at the i-th row and j-th column is (i + 1) * (j + 1) (if 0-indexed) or i * j (if 1-indexed).
+//
+//Given integers N and X, write a function that returns the number of times X appears as a value in an N by N multiplication table.
+public class Findmultiplication_21_06_2022 {
+
+    public static int integer(int n,int x){
+        int[][] arr= new int[n][n];
+        int count=0;//counter to count the number of ways.
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                arr[i][j]=(i+1)*(j+1);
+                if(x==arr[i][j]){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args){
+        int n=6;
+        int x=12;
+        int a= integer(6,12);
+        System.out.println(a);
+    }
+}
