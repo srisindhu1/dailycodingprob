f22d47b48b9a06ce6e6a5509d7336ed328c2ce2f
srisindhu1 <104442042+srisindhu1@users.noreply.github.com>
2022-06-22 18:03:14 +0530

    problem75test

---
+++ src/test/java/org/example/Longestsequence_22_06_2022test.java
@@ -0,0 +1,13 @@
+package dailycodingproblems;
+
+import org.junit.jupiter.api.*;
+
+
+public class Longestsequence_22_06_2022test {
+    @Test
+    public void test1(){
+        int[] nums={3,10,2,11};
+        int n=4;
+        System.out.println(Longestsequence_22_06_2022.solution(nums,n));
+    }
+}
